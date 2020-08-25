7e395d660ea1d246ef3f201e1e696701fb395046
Grace <wanenegrace@gmail.com>
2020-08-25 14:11:20 +0300

    Added previous quote

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,4 +9,18 @@ import {Quote} from './quote';
 export class AppComponent {
   quoteModel = new Quote('Steve Jobs', 'If you really look closely, most overnight successes took a long time.', 'Grace', '2020-8-22')
 
+  public previousAuthor : string = "Steve Jobs"
+  public previousQuote: string = "If you really look closely, most overnight successes took a long time."
+  public previousSubmitter: string = "Grace"
+  public previousDate: string = "25/08/2020"
+
+  numberOfVotes : number = 0;
+
+  upvoteButtonClick(){
+    this.numberOfVotes++;
+  }
+
+  downvoteButtonClick(){
+    this.numberOfVotes--;
+  }
 }
