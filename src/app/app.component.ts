dd2e01f0208a5dc7aa4984e906e45037579de6cf
Grace <wanenegrace@gmail.com>
2020-08-25 13:54:42 +0300

    Added Quote Class & Model

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import {Quote} from './quote';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,6 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'tdf';
+  quoteModel = new Quote('Steve Jobs', 'If you really look closely, most overnight successes took a long time.', 'Grace', '2020-8-22')
+
 }
---
+++ src/app/quote.ts
@@ -0,0 +1,8 @@
+export class Quote {
+  constructor(
+    public author: string;
+    public quote: string;
+    public submitter: string;
+    public date: string;
+  ){}
+}
